5b926cd5077c71af51669141e528919f93e474d4
angeloevangelista <angeloevan.ane@gmail.com>
2020-06-21 18:51:10 -0300

    Alterada sequencia de verificação

--- middleware/authWithToken.js
+++ middleware/authWithToken.js
@@ -2,27 +2,34 @@ const jwt = require('jsonwebtoken');
 const JWTSecret = 'ID68(@WCPw|uzY5*';
 
 function authToken(req, res, next) {
-    let token = req.headers['authorization'];
-    token = token.split(' ')[1];
-    
-    if(token) {
-        jwt.verify(token, JWTSecret, (err, data) => {
-            if(err) {
-                res.status(401);
-                res.json({err: 'Token inválido!'});
-            } else {
-                req.token = token;
-                req.loggedUser = {
-                    id: data.id,
-                    email: data.email
-                };
-                next();
-            }
-        });
-    } else {
-        res.status(401);
-        res.json({err: 'Token inválido!'});
+    const bearerToken = req.headers['authorization'];
+
+    if (!bearerToken) {
+        return res.status(401).json({ err: 'Token não fornecido' });
     }
+
+    const token = bearerToken.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ err: 'Token inválido' });
+    }
+
+    jwt.verify(token, JWTSecret, (err, data) => {
+        if (err) {
+            return res.status(401).json({ err: 'Token inválido' });
+        }
+
+        const { id, email } = data;
+
+        req.token = token;
+
+        req.loggedUser = {
+            id,
+            email,
+        };
+
+        return next();
+    });
 }
 
 module.exports = {
